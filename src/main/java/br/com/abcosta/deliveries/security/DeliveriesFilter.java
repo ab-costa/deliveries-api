476da4f1d6b18f623b48e67e4d7088f90d876453
ab-costa <costa.alessandroborges@gmail.com>
2022-03-26 15:39:45 -0300

    feature: finalização das configurações de segurança

--- src/main/java/br/com/abcosta/deliveries/security/DeliveriesFilter.java
+++ src/main/java/br/com/abcosta/deliveries/security/DeliveriesFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class DeliveriesFilter extends OncePerRequestFilter {
@@ -15,7 +17,13 @@ public class DeliveriesFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("REQUISÇÃO PASSANDO PELO FILTRO");
+		System.out.println(">>>>>>> REQUISÇÃO PASSANDO PELO FILTRO");
+		
+		if (request.getHeader("Authorization") != null) {
+			Authentication authentication = TokenUtil.validateToken(request);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		
 		filterChain.doFilter(request, response);
 	}
 }
---
+++ src/main/java/br/com/abcosta/deliveries/security/TokenUtil.java
@@ -0,0 +1,79 @@
+package br.com.abcosta.deliveries.security;
+
+import java.security.Key;
+import java.util.Collections;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import br.com.abcosta.deliveries.model.DeliveryMan;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class TokenUtil {
+	
+	private static final int SEGUNDOS = 1000;
+	private static final int MINUTOS = 60 * SEGUNDOS;
+	private static final int HORAS = 60 * MINUTOS;
+	private static final int DIAS = 24 * HORAS;
+
+	private static final String HEADER = "Authorization";
+	private static final String PREFIX = "Bearer ";
+	private static final long EXPIRATION = 2 * MINUTOS;
+	private static final String SECRET_KEY = "83b483a36193c3ce03c8cec39df0db8e";
+	private static final String ISSUER = "83b483a36193c3ce03c8cec39df0db8e";
+
+
+	public static String createToken(DeliveryMan deliveryMan) {
+		Key secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+		
+		String token = Jwts.builder().setSubject(deliveryMan.getName())
+										.setIssuer(ISSUER)
+										.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+										.signWith(secretKey, SignatureAlgorithm.HS256)
+										.compact();
+		
+		System.out.println("TOKEN: " + token);
+		
+		return PREFIX + token;
+	}
+	
+	private static boolean isExpiratioDateValid(Date expirationDate) {
+		return expirationDate.after(new Date(System.currentTimeMillis()));
+	}
+	
+	private static boolean isIssuerValid(String issuer) {
+		return issuer.equals(ISSUER);
+	}
+	
+	private static boolean isSubjectValid(String subject) {
+		return subject != null && subject.length() > 0;
+	}
+	
+	public static Authentication validateToken(HttpServletRequest request) {
+		
+		String token = request.getHeader(HEADER);
+		
+		token = token.replace(PREFIX, "");
+		
+		Jws<Claims> jwsClaims = Jwts.parserBuilder().setSigningKey(SECRET_KEY.getBytes())
+												.build()
+												.parseClaimsJws(token);
+		
+		Date expirationDate = jwsClaims.getBody().getExpiration();
+		String issuer = jwsClaims.getBody().getIssuer();
+		String name = jwsClaims.getBody().getSubject();
+		
+		if(isExpiratioDateValid(expirationDate) && isIssuerValid(issuer) && isSubjectValid(name)) {
+			return new UsernamePasswordAuthenticationToken(name, null, Collections.emptyList());
+		}
+		
+		return null;
+	}
+}
