446dbe234b4505c48e21f6916a3afcf912b67bae
ab-costa <costa.alessandroborges@gmail.com>
2022-03-26 14:16:06 -0300

    fix: inclusão do construtores na classe Token

--- src/main/java/br/com/abcosta/deliveries/security/Token.java
+++ src/main/java/br/com/abcosta/deliveries/security/Token.java
@@ -3,6 +3,17 @@ package br.com.abcosta.deliveries.security;
 public class Token {
 
 	private String token;
+	
+	
+	public Token() {
+		super();
+	}
+
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
 
 	public String getToken() {
 		return token;
