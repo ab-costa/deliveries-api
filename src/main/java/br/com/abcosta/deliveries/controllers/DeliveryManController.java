c04e52bbf85746c8e81d74d1de002f6c3d75ca20
ab-costa <costa.alessandroborges@gmail.com>
2022-03-26 00:50:07 -0300

    feature: endpoints para Deliveryman(criar, buscar por id, deletar por id)

--- src/main/java/br/com/abcosta/deliveries/controllers/DeliveryManController.java
+++ src/main/java/br/com/abcosta/deliveries/controllers/DeliveryManController.java
@@ -1,9 +1,14 @@
 package br.com.abcosta.deliveries.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.abcosta.deliveries.dao.DeliveryManDAO;
@@ -15,8 +20,24 @@ public class DeliveryManController {
 	@Autowired
 	private DeliveryManDAO dao;
 	
+	
 	@GetMapping("/entregadores")
 	public List<DeliveryMan> couriersList() {
 		return (List<DeliveryMan>)dao.findAll();
+	}
+	
+	@GetMapping("/entregadores/{id}")
+	public Optional<DeliveryMan> getDeliveryMan(@PathVariable Integer id) {
+		return dao.findById(id);
+	}
+	
+	@PostMapping("/entregadores")
+	public DeliveryMan createDeliveryMan(@RequestBody DeliveryMan deliveryMan) {
+		return dao.save(deliveryMan);
+	}
+	
+	@DeleteMapping("/entregadores/{id}")
+	public void deleteDeliveryMan(@PathVariable Integer id) {
+		dao.deleteById(id);
 	}
 }
